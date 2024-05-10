2c032c4e692e7f004da4f0f573eac8e9aa048849
AAD23110162 <159966137+AAD23110162@users.noreply.github.com>
2024-05-10 06:21:52 +0000

    Version 5

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,8 @@ int main(int argc, char const *argv[])
     Archivo nave("./assets/images/nave.txt");
     Archivo margen3("./assets/images/margen3.txt");
     Archivo margen4("./assets/images/margen4.txt");
+    Archivo bala1("./assets/images/bala1.txt");
+    Archivo bala2("./assets/images/bala2.txt");
     list<Dibujo> dibujos2;
 
     // Vector para almacenar las posiciones iniciales de cada dibujo
@@ -41,13 +43,17 @@ int main(int argc, char const *argv[])
     posicionesIniciales1.push_back({0, 0}); // Posición inicial del margen2
     posicionesIniciales1.push_back({175, 0}); // Posición inicial del margen2.1
 
-    posicionesIniciales2.push_back({60, 9}); // Posición inicial del numero de nivel
-    posicionesIniciales2.push_back({40, 25}); // Posición inicial de la nave
-    posicionesIniciales2.push_back({50, 25}); // Posición inicial del ovni 1
-    posicionesIniciales2.push_back({60, 25}); // Posición inicial del ovni 2
+    posicionesIniciales2.push_back({70, 15}); // Posición inicial del numero de nivel
+    posicionesIniciales2.push_back({-40, 25}); // Posición inicial de la nave
+    posicionesIniciales2.push_back({-20, 25}); // Posición inicial del ovni 1
+    posicionesIniciales2.push_back({-10, 25}); // Posición inicial del ovni 2
     posicionesIniciales2.push_back({0, 0}); // Posición inicial del margen3
     posicionesIniciales2.push_back({0, 0}); // Posición inicial del margen4
     posicionesIniciales2.push_back({175, 0}); // Posición inicial del margen4.1
+    
+    posicionesIniciales2.push_back({-40, 25}); // Bala Nave
+    posicionesIniciales2.push_back({-17, 25}); // Bala ovni1
+    posicionesIniciales2.push_back({-7, 25}); // Bala ovni1
 
     for (size_t i = 0; i < 1; i++)
     {
@@ -116,6 +122,18 @@ int main(int argc, char const *argv[])
         dibujos2.push_back(d);
     }
 
+    for (size_t i = 0; i < 1; i++)
+    {
+        Dibujo d = bala1.CrearDibujo();
+        dibujos2.push_back(d);
+    }
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        Dibujo d = bala2.CrearDibujo();
+        dibujos2.push_back(d);
+    }
+
     auto Pantalla = Screen::Create(
     Dimension::Fixed(176),  // Ancho fijo de 200 píxeles
     Dimension::Fixed(36)   // Alto fijo de 100 píxeles
@@ -125,6 +143,9 @@ int main(int argc, char const *argv[])
     vector<pair<int, int>> desplazamientos1(dibujos1.size(), {0, 0});
     vector<pair<int, int>> desplazamientos2(dibujos1.size(), {0, 0});
     vector<pair<int, int>> desplazamientos3(dibujos2.size(), {0, 0});
+    vector<pair<int, int>> desplazamientos4(dibujos2.size(), {0, 0});
+    vector<pair<int, int>> desplazamientos5(dibujos2.size(), {0, 0});
+    vector<pair<int, int>> desplazamientos6(dibujos2.size(), {0, 0});
 
     // Desplazamiento específico para la nave uno (índice 0) y la nave dos (índice 1)
     desplazamientos1[0] = {1, 0}; // La nave uno se mueve 2 posiciones hacia la derecha
@@ -145,14 +166,54 @@ int main(int argc, char const *argv[])
     desplazamientos2[5] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
     desplazamientos2[6] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
 
-    desplazamientos3[1] = {1, 0}; // La nave dos se mueve 4 posiciones hacia abajo
-    desplazamientos3[2] = {1, 0}; // La nave dos se mueve 4 posiciones hacia abajo
-    desplazamientos3[3] = {1, 0}; // La nave dos se mueve 4 posiciones hacia abajo
-    desplazamientos3[4] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos3[0] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos3[1] = {2, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos3[2] = {2, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos3[3] = {2, 0}; // La nave dos se mueve 4 posiciones hacia abajo
 
+    desplazamientos3[4] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
     desplazamientos3[5] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
     desplazamientos3[6] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
-    desplazamientos3[7] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos3[7] = {2, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos3[8] = {2, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos3[9] = {2, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+
+    desplazamientos4[0] = {5,1}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos4[1] = {-10, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos4[2] = {-10, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos4[3] = {-10, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+
+    desplazamientos4[4] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos4[5] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos4[6] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos4[7] = {-10, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos4[8] = {-10, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos4[9] = {-10, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+
+    desplazamientos5[0] = {0,0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos5[1] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos5[2] = {-2, -1}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos5[3] = {0, -1}; // La nave dos se mueve 4 posiciones hacia abajo
+
+    desplazamientos5[4] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos5[5] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos5[6] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos5[7] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos5[8] = {-2, -1}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos5[9] = {0, -1}; // La nave dos se mueve 4 posiciones hacia abajo
+
+    desplazamientos6[0] = {0,0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos6[1] = {-1, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos6[2] = {1, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos6[3] = {1, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+
+    desplazamientos6[4] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos6[5] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos6[6] = {0, 0}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos6[7] = {2, -1}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos6[8] = {1, 1}; // La nave dos se mueve 4 posiciones hacia abajo
+    desplazamientos6[9] = {-1, 1}; // La nave dos se mueve 4 posiciones hacia abajo
+
 
     // Establecer las posiciones iniciales de cada dibujo
     size_t index = 0;
@@ -191,7 +252,7 @@ int main(int argc, char const *argv[])
 
     this_thread::sleep_for(2s);
 
-    for (size_t i = 0; i < 40; i++)
+    for (size_t i = 0; i < 30; i++)
     {
         
         Pantalla.Clear();
@@ -224,7 +285,59 @@ int main(int argc, char const *argv[])
     index2++;
     }
 
-    for (size_t i = 0; i < 80; i++)
+    for (size_t i = 0; i < 120; i++)
+    {
+        
+        Pantalla.Clear();
+        this_thread::sleep_for(0.025s);
+
+        // Actualizar
+        size_t index2 = 0;
+        for (auto &&dibujo : dibujos2)
+        {
+            // Aplicar desplazamientos individuales
+            dibujo.DesplazarX(desplazamientos3[index2].first);
+            dibujo.DesplazarY(desplazamientos3[index2].second);
+            index2++;
+        }
+
+        // Dibujar
+        for (auto &&dibujo : dibujos2)
+        {
+            dibujo.Dibujar(Pantalla);
+        }
+        
+        Pantalla.Print();
+        cout << Pantalla.ResetPosition();          
+    }
+
+    for (size_t i = 0; i < 14; i++)
+    {
+        
+        Pantalla.Clear();
+        this_thread::sleep_for(0.025s);
+
+        // Actualizar
+        size_t index2 = 0;
+        for (auto &&dibujo : dibujos2)
+        {
+            // Aplicar desplazamientos individuales
+            dibujo.DesplazarX(desplazamientos4[index2].first);
+            dibujo.DesplazarY(desplazamientos4[index2].second);
+            index2++;
+        }
+
+        // Dibujar
+        for (auto &&dibujo : dibujos2)
+        {
+            dibujo.Dibujar(Pantalla);
+        }
+        
+        Pantalla.Print();
+        cout << Pantalla.ResetPosition();          
+    }
+
+        for (size_t i = 0; i < 22; i++)
     {
         
         Pantalla.Clear();
@@ -235,8 +348,34 @@ int main(int argc, char const *argv[])
         for (auto &&dibujo : dibujos2)
         {
             // Aplicar desplazamientos individuales
-            dibujo.DesplazarX(desplazamientos3[index2].first);
-            dibujo.DesplazarY(desplazamientos3[index2].second);
+            dibujo.DesplazarX(desplazamientos5[index2].first);
+            dibujo.DesplazarY(desplazamientos5[index2].second);
+            index2++;
+        }
+
+        // Dibujar
+        for (auto &&dibujo : dibujos2)
+        {
+            dibujo.Dibujar(Pantalla);
+        }
+        
+        Pantalla.Print();
+        cout << Pantalla.ResetPosition();          
+    }
+
+     for (size_t i = 0; i < 32; i++)
+    {
+        
+        Pantalla.Clear();
+        this_thread::sleep_for(0.1s);
+
+        // Actualizar
+        size_t index2 = 0;
+        for (auto &&dibujo : dibujos2)
+        {
+            // Aplicar desplazamientos individuales
+            dibujo.DesplazarX(desplazamientos6[index2].first);
+            dibujo.DesplazarY(desplazamientos6[index2].second);
             index2++;
         }
 
